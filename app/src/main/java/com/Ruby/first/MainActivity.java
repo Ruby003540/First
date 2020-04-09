f5680b52e3c30ea8992cd4b722fc7e5d96118896
Ruby003540 <2603867043@qq.com>
2020-04-09 16:17:30 +0800

    温度转换器，一个球队的记分牌

--- app/src/main/java/com/Ruby/first/MainActivity.java
+++ app/src/main/java/com/Ruby/first/MainActivity.java
@@ -8,17 +8,32 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView out;
+    EditText inp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
+        inp=(EditText) findViewById(R.id.input);
+        out=(TextView)findViewById(R.id.output);
 
+    }
+    public void onClick(View btn) {
+        Log.i("main","computing....");
+
+        String str=(String)inp.getText().toString();
+        double c=Double.valueOf(str);
+        double f=c*1.8+32;
+        out.setText("结果为："+f);
+    }
 }
