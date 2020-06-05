9fb49da35cfbfb884537d52162c4f7e5487e099e
Ruby003540 <2603867043@qq.com>
2020-06-05 17:00:05 +0800

    Grid列表

--- app/src/main/java/com/Ruby/first/MyListActivity.java
+++ app/src/main/java/com/Ruby/first/MyListActivity.java
@@ -7,15 +7,22 @@ import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MyListActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MyListActivity extends AppCompatActivity {
+    List<String> data = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_list);
 
         ListView listview=(ListView)findViewById(R.id.mylist);
-        String data[]={"111","222"};
+
+        for(int i=0;i<10;i++){
+            data.add("item"+i);
+        }
+
         ListAdapter adapter=new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,data);//android.R.layout.simple_expandable_list_item_1安卓平台提供的列表布局
         listview.setAdapter(adapter);//对控件使用adapter
     }
